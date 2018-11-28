b145bc44430dba52715ddffcec123f791de38605
Andrew Lever <andylever28@gmail.com>
2018-11-27 22:03:50 -0500

    add font awesome/google. Responsive nav + promo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
-// <img src={require('../images/medici_homepage.jpg')} alt=''></img>
-
 
 const NavBar = (props) => {
 
+  function testing () {
+    let navWrapper = document.querySelector('.navbar-wrapper-right')
+    navWrapper.style.display = navWrapper.style.display === 'flex' ? 'none' : 'flex';
+  }
+
   return (
-    <ul class="topnav">
-      <li>
-        <NavLink to='/' >
-          Muff Diver Apparel
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/home" >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/about">
-          About Us
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/contact">
-          Contact Us
-        </NavLink>
-      </li>
-    </ul>
+    <div className="navbar-container">
+      <div className="navbar-promo">
+        <p className="no-margin text-center">Free Shipping on all orders.</p>
+      </div>
+      <div className="navbar">
+        <div className="navbar-toggle">
+          <i onClick={testing} className="fas fa-bars"></i>
+        </div>
+        <nav className="navbar-wrapper text-center navbar-wrapper-left">
+          <div className="nav-link text-large">
+            <NavLink to='/' >
+              Muff Diver Apparel
+            </NavLink>
+          </div>
+        </nav>
+        <nav className="navbar-wrapper navbar-wrapper-right">
+          <div className="nav-link">
+            <NavLink to="/home" >
+              Home
+            </NavLink>
+          </div>
+          <div className="nav-link">
+            <NavLink to="/about">
+              About Us
+            </NavLink>
+          </div>
+          <div className="nav-link">
+            <NavLink to="/contact">
+              Contact Us
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+    </div>
   );
 }
 
